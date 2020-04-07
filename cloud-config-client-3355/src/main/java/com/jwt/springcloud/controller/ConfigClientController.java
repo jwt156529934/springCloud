95d653d69e882aac7aef5ee0785cad8d063babb6
jiwt <156529934@qq.com>
2020-04-07 17:54:46 +0800

    config配置中心

---
+++ cloud-config-client-3355/src/main/java/com/jwt/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,19 @@
+package com.jwt.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String configInfo;  //要访问的3344上的信息
+
+    @GetMapping("/configInfo")	//请求地址
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
